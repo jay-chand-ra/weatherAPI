36625e7f4a840f4a94eae3c1b52746be83c86da5
boyajaychandra@gmail.com <raviteja.nagalla@webileapps.com>
2024-10-20 10:49:55 +0530

    Prepare for deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Container, Box, ThemeProvider, createTheme, CssBaseline, IconButton, AppBar, Toolbar, Typography } from '@mui/material';
 import { teal, amber, blue } from '@mui/material/colors';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
